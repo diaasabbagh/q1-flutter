59a8ad42267c35bfd89b7fbfa23de5aa12acd876
diayilmaz <diaasabbagh1234@gmai.com>
2023-05-24 11:56:22 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: LocationApp(),
+    );
+  }
+}
+
+class LocationApp extends StatefulWidget {
+  const LocationApp({Key? key}) : super(key: key);
+
+  @override
+  State<LocationApp> createState() => _LocationAppState();
+}
+
+class _LocationAppState extends State<LocationApp> {
+  var locationMessage = "";
+
+  void getCurrentLocation() async {
+    var position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    var lastPosition = await Geolocator().getLastKnownPosition();
+    print(lastPosition);
+    var lat = position.latitude;
+    var long = position.longitude;
+    print("$lat ,$long");
+    setState(() {
+      locationMessage = "Latitude : $lat,Longitude : $long";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Location Services"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.location_on,
+              size: 46.0,
+              color: Colors.blue,
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            const Text(
+              "Get User Location",
+              style: TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Text(locationMessage),
+            TextButton(
+                onPressed: () {
+                  getCurrentLocation();
+                },
+                child: const Text(
+                  "Get Current Location",
+                  style: TextStyle(
+                    color: Colors.blue,
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
